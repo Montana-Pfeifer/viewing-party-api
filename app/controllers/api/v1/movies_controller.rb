e6da1f80af76405560b5060db5e3e4e47a51be60
Montana Pfeifer <montanapfeifer@hotmail.com>
2025-03-28 15:03:15 -0600

    Movie search complete/ writing tests. Tested with postman

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,9 +1,24 @@
 class Api::V1::MoviesController < ApplicationController
     def index
-        top_movies = MovieGateway.fetch_top_movies
+        if params[:query].present?
 
-        formatted_movies = MovieSerializer.format_movies(top_movies)
+            searched_movies = MovieGateway.find_movie(params[:query])
+        else
+            searched_movies = MovieGateway.fetch_top_movies
+        end
+
+        formatted_movies = MovieSerializer.format_movies(searched_movies)
 
         render json: formatted_movies
     end
+
+    # def show
+    #     query = params[:query]
+
+    #     searched_movies = MovieGateway.find_movie(query)
+
+    #     formatted_movies = MovieSerializer.format_movies(searched_movies)
+
+    #     render json: formatted_movies
+    # end
 end
